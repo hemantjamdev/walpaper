798aa1858074ca19ea6fd2df355ccb0379024a1a
Hemant jam <60656624+hemantjamdev@users.noreply.github.com>
2023-03-24 12:51:08 +0530

    dependencies added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:wallpaper/view/splash_page.dart';
-
 
 void main(){
   WidgetsFlutterBinding.ensureInitialized();
