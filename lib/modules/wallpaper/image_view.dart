219f56139b9c6882dc9123d8bb5a678d853b51c1
Hemant jam <60656624+hemantjamdev@users.noreply.github.com>
2023-03-27 01:13:39 +0530

    set as wallpaper fearure

--- lib/modules/wallpaper/image_view.dart
+++ lib/modules/wallpaper/image_view.dart
@@ -1,7 +1,7 @@
-
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:wallpaper/modules/wallpaper/image_view_provider.dart';
@@ -28,46 +28,103 @@ class ImageView extends StatelessWidget {
         body: Stack(
           children: [
             InteractiveViewer(child: WallpaperGrid(url: imageUrl)),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  child: Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        CustomIcon(
-                          icon: Icon(Icons.share),
-                          onTap: () => controller.shareImage(imageUrl),
-                        ),
-                        CustomIcon(
-                            icon: Icon(
-                              Icons.file_download,
-                            ),
-                            onTap: () async {
-                              if (await Permissions.getPermission()) {
-                                controller.downloadImage(imageUrl);
-                                log("------> permission given<-------");
-                              } else {
-                                customSnackBar(
-                                    msg: "permission is required for download");
-                              }
-                            }),
-                        CustomIcon(
-                          icon: Icon(Icons.wallpaper),
-                          onTap: () => controller.setWallpaper(
-                            imageUrl,
-                            WallpaperManagerFlutter.HOME_SCREEN,
-                          ),
-                        ),
-                        CustomIcon(icon: Icon(Icons.favorite)),
-                      ],
+            Obx(
+              () => Container(
+                color: Colors.black
+                    .withOpacity(controller.isShowingLoading.value ? 0.5 : 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: controller.isShowingLoading.value
+                          ? Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: Center(child: LinearProgressIndicator()),
+                            )
+                          : Container(),
                     ),
-                  ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          CustomIcon(
+                            icon: Icon(Icons.share),
+                            onTap: () => controller.shareImage(imageUrl),
+                          ),
+                          CustomIcon(
+                              icon: Icon(Icons.file_download),
+                              onTap: () async {
+                                if (await Permissions.getPermission()) {
+                                  controller.downloadImage(imageUrl);
+                                } else {
+                                  customSnackBar(
+                                      msg:
+                                          "permission is required for download");
+                                }
+                              }),
+                          CustomIcon(
+                              icon: Icon(Icons.wallpaper),
+                              onTap: () async {
+                                dynamic location = "";
+                                showModalBottomSheet(
+                                        context: context,
+                                        builder: (context) {
+                                          return Container(
+                                            child: Column(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                ListTile(
+                                                  title:
+                                                      Text('Set as homescreen'),
+                                                  onTap: () {
+                                                    location =
+                                                        WallpaperManagerFlutter
+                                                            .HOME_SCREEN;
+                                                    Get.back();
+                                                  },
+                                                ),
+                                                ListTile(
+                                                  title:
+                                                      Text('Set as lockscreen'),
+                                                  onTap: () {
+                                                    location =
+                                                        WallpaperManagerFlutter
+                                                            .LOCK_SCREEN;
+                                                    Get.back();
+                                                  },
+                                                ),
+                                                ListTile(
+                                                  title: Text('Set as both'),
+                                                  onTap: () {
+                                                    location =
+                                                        WallpaperManagerFlutter
+                                                            .BOTH_SCREENS;
+                                                    Get.back();
+                                                  },
+                                                )
+                                              ],
+                                            ),
+                                          );
+                                        })
+                                    .then((value) => controller.setWallpaper(
+                                        imageUrl, location));
+
+                              }),
+                          CustomIcon(
+                            icon: Icon(Icons.favorite),
+                            onTap: () {
+                              // controller.showLoading();
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             )
           ],
         ),
--- lib/modules/wallpaper/image_view_provider.dart
+++ lib/modules/wallpaper/image_view_provider.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_file_downloader/flutter_file_downloader.dart';
@@ -10,59 +9,79 @@ import 'package:wallpaper/widgets/custon_toast.dart';
 import 'package:wallpaper_manager_flutter/wallpaper_manager_flutter.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
-import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
+import 'package:image_picker/image_picker.dart' as image_picker;
 
 class ImageViewController extends GetxController {
+  RxBool isShowingLoading = false.obs;
+  XFile? downloadedImage;
+
   Future<void> setWallpaper(String imageUrl, location) async {
     var file = await DefaultCacheManager().getSingleFile(imageUrl);
-    try {
-      Get.dialog(Container(color: Colors.white.withOpacity(0.2)),
-          barrierDismissible: false);
-
-      WallpaperManagerFlutter().setwallpaperfromFile(file, location);
-      Timer(Duration(seconds: 2), () {
-        Get.back();
-        customSnackBar(msg: 'wallpaper updated');
-      });
-    } catch (e) {
-      customSnackBar(msg: 'Error Setting Wallpaper');
+    if (location != "" && location != null) {
+      try {
+        isShowingLoading.value = true;
+        WallpaperManagerFlutter().setwallpaperfromFile(file, location);
+        Timer(Duration(seconds: 2), () {
+          isShowingLoading.value = false;
+          customSnackBar(msg: 'wallpaper updated');
+        });
+      } catch (e) {
+        customSnackBar(msg: 'Error Setting Wallpaper');
+        isShowingLoading.value = false;
+      }
     }
   }
 
   Future<void> shareImage(String text) async {
-    await Share.share(text);
+    isShowingLoading.value = true;
+    if (downloadedImage != null) {
+      Share.shareXFiles([downloadedImage!]);
+    } else {
+      await downloadImage(text);
+      if (downloadedImage != null) {
+        Share.shareXFiles([downloadedImage!]);
+      }
+    }
+    isShowingLoading.value = false;
   }
 
   Future<void> downloadImage(String imageUrl) async {
     Directory? directory = await path_provider.getExternalStorageDirectory();
     if (directory!.existsSync()) {
-      await FileDownloader.downloadFile(
-          url: imageUrl,
-          onProgress: (String? fileName, double progress) {},
-          onDownloadCompleted: (String path) {},
-          onDownloadError: (String error) {});
+      try {
+        await FileDownloader.downloadFile(
+            url: imageUrl,
+            onProgress: (String? fileName, double progress) {
+              isShowingLoading.value = true;
+            },
+            onDownloadCompleted: (String path) {
+              setToFile(path);
+              isShowingLoading.value = false;
+              // customSnackBar(msg: 'already downlaoded');
+            },
+            onDownloadError: (String error) {
+              isShowingLoading.value = false;
+              //customSnackBar(msg: 'already downlaoded');
+            });
+      } catch (e) {
+        customSnackBar(msg: 'Error in download');
+      }
     } else {
       FileDownloader.downloadFile(
           url: imageUrl,
-          onProgress: (String? fileName, double progress) {},
-          onDownloadCompleted: (String path) {},
+          onProgress: (String? fileName, double progress) {
+            isShowingLoading.value = true;
+          },
+          onDownloadCompleted: (String path) {
+            setToFile(path);
+            isShowingLoading.value = false;
+          },
           onDownloadError: (String error) {});
     }
   }
-}
 
-void showLoading() {
-  LiquidCircularProgressIndicator(
-    value: 0.25,
-    // Defaults to 0.5.
-    valueColor: AlwaysStoppedAnimation(Colors.pink),
-    // Defaults to the current Theme's accentColor.
-    backgroundColor: Colors.white,
-    // Defaults to the current Theme's backgroundColor.
-    borderColor: Colors.red,
-    borderWidth: 5.0,
-    direction: Axis.horizontal,
-    // The direction the liquid moves (Axis.vertical = bottom to top, Axis.horizontal = left to right). Defaults to Axis.vertical.
-    center: Text("Loading..."),
-  );
+  setToFile(String bytes) {
+    downloadedImage =
+        image_picker.XFile.fromData(Uint8List.fromList(bytes.codeUnits));
+  }
 }
